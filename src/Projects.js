0b1bf4e51347edb9411505fee025080c475e58e8
Ahmed Keshka <up962500@myport.ac.uk>
2024-06-15 16:49:48 +0300

    bug fix

--- src/Projects.js
+++ src/Projects.js
@@ -4,7 +4,7 @@ import styles from "./css/Projects.module.css";
 
 function Projects() {
   const [projectStates, setProjectStates] = useState({
-    firstProject: false,
+    firstProject: true,
     secondProject: false,
     thirdProject: false,
   });
